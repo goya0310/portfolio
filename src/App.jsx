6c0a842384d74bac7956d3ee22f14f78bba018ec
Diego Llaya <goya0310@gmail.com>
2023-04-01 04:16:48 -0300

    cambios para responsive

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
         <Hero />
       </div>
       <About />
-      {/* <Tech /> */}
+      <Tech />
       <Works />
       <div className="relative z-0">
         <Contact />
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import {
   Decal,
@@ -9,26 +9,32 @@ import {
 } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
+import { PlaneGeometry } from "three";
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
+  const isMobile = props.isMobile;
 
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
-        <sphereGeometry args={[1, 16, 16]} />
+        {isMobile ? (
+          <planeGeometry attach="geometry" args={[2, 2]} color="0x800080" />
+        ) : (
+          <sphereGeometry args={[1, 80, 80]} />
+        )}
         <meshStandardMaterial
-          color='#fff8eb'
+          color="#fff8eb"
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
         />
         <Decal
-          position={[0, 0, 1]}
+          position={isMobile ? [0, 0, 0] : [0, 0, 1]}
           rotation={[2 * Math.PI, 0, 6.25]}
-          scale={1}
+          scale={isMobile? 1.5 : 1}
           map={decal}
           flatShading
         />
@@ -38,15 +44,38 @@ const Ball = (props) => {
 };
 
 const BallCanvas = ({ icon }) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
-      frameloop='demand'
+      frameloop="demand"
       dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} />
-        <Ball imgUrl={icon} />
+        <Ball imgUrl={icon} isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
