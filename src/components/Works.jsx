4275fd18582d256a0986bfc595b843c168ab4397
Diego Llaya <goya0310@gmail.com>
2023-09-19 14:25:45 -0300

    cards cursor pointer and github icon hover

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -34,34 +34,37 @@ const ProjectCard = ({
           scale: 1,
           speed: 450,
         }}
-        className="bg-blue p-5 rounded-2xl sm:w-[360px] w-full cursor-pointer shadow-card"
-        onClick={() => handleClick()}
+        className="bg-blue p-5 rounded-2xl sm:w-[360px] w-full shadow-card"
       >
-        <div className="relative w-full h-[230px]">
+        <div
+          className="relative w-full h-[230px] cursor-pointer"
+          onClick={() => handleClick()}
+        >
           <img
             src={clicked ? imageGif : image}
             alt="project_image"
-            className="w-full h-full object-cover rounded-2xl"
+            className="w-full h-full object-cover rounded-2xl "
           />
 
           <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
-            <div
+            <motion.div
               onClick={() => window.open(source_code_link, "_blank")}
               className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+              whileHover={{ scale: 1.2 }}
             >
-              <img
+              <img 
                 src={github}
                 alt="source code"
                 className="w-1/2 h-1/2 object-contain"
               />
-            </div>
+            </motion.div>
           </div>
         </div>
 
         <div className="mt-5">
           <h3 className="text-white font-bold text-[24px]">{name}</h3>
           <p className="mt-2 text-secondary text-[14px]">
-            {isChecked? descriptionEn : description}
+            {isChecked ? descriptionEn : description}
           </p>
         </div>
 
